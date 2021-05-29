1a05c3603bc8e95aebf7f19a4c7fe53df8e5373c
miro junker <miro.junker@gmail.com>
2021-05-29 21:12:37 +0200

    Add input form

--- src/TipCalculator/TipCalculator.tsx
+++ src/TipCalculator/TipCalculator.tsx
@@ -5,6 +5,24 @@ const TipCalculator: React.FC = () => {
   return (
     <div className={styles.root}>
       <h1>TipCalculator</h1>
+
+      <fieldset>
+        <h3>Bill value</h3>
+        <input type="number" />
+
+        <h3>Staff quality</h3>
+        <input type="radio" name="satisfaction" id="bad" />
+        <label htmlFor="bad">Bad</label>
+        <input type="radio" name="satisfaction" id="good" />
+        <label htmlFor="good">Good</label>
+        <input type="radio" name="satisfaction" id="perfect" />
+        <label htmlFor="perfect">Perfect</label>
+
+        <h3>People involved</h3>
+        <input type="number" />
+      </fieldset>
+
+      <button>Calculate</button>
     </div>
   );
 }
