6ecc1892db6eceaabe07fd7b0629f2eca37a56d2
miro junker <miro.junker@gmail.com>
2021-05-29 22:02:16 +0200

    Move text-translations outside UI component

--- src/TipCalculator/TipCalculator.tsx
+++ src/TipCalculator/TipCalculator.tsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import styles from './TipCalculator.module.scss';
 import { satisfactionOptions } from './data'
-
+import t from './translations'
 
 const TipCalculator: React.FC = () => {
   return (
     <div className={styles.root}>
-      <h1>TipCalculator</h1>
+      <h1>{t.title}</h1>
 
-      <fieldset>
-        <h3>Bill value</h3>
-        <input type="number" />
+      <form>
+        <fieldset>
+          <h3>{t.bill}</h3>
+          <input type='number' />
 
-        <h3>Staff quality</h3>
+          <h3>{t.satisfaction}</h3>
 
-        {satisfactionOptions.map((option) => {
-          return (
-            <div key={option.name}>
-              <input type="radio" name="satisfaction" id={option.name} />
-              <label htmlFor={option.name}>{option.label}</label>
-            </div>
-          );
-        })}
+          {satisfactionOptions.map((option) => {
+            return (
+              <div key={option.name}>
+                <input type='radio' name='satisfaction' id={option.name} />
+                <label htmlFor={option.name}>{option.label}</label>
+              </div>
+            );
+          })}
 
-        <h3>People involved</h3>
-        <input type="number" />
-      </fieldset>
+          <h3>{t.peopleCount}</h3>
+          <input type='number' />
+        </fieldset>
 
-      <button>Calculate</button>
+        <button type='submit'>
+          {t.submit}
+        </button>
+      </form>
     </div>
   );
 }
---
+++ src/TipCalculator/translations.ts
@@ -0,0 +1,10 @@
+const translations = {
+  title: 'Tip Calculator',
+  bill: 'Bill value',
+  satisfaction: 'Staff quality',
+  peopleCount: 'Persons involved',
+  submit: 'Calculate',
+}
+
+
+export default translations
